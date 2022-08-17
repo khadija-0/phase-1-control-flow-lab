9ef6426bea789ca1f62f1c00582516e34226e61d
khadija.dahir <khadija.dahir@student.moringaschool.com>
2022-08-17 20:02:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return 'This one is on me!'
+  }
+  else if (ride > 2000 && ride < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (ride >2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+
+// ternary
+function ternaryCheckCity(city){
   // Write your code here!
+  let result = (city === 'NYC')? 'Ok, sounds good.': 'No go.';
+  return result
 }
 
-function switchOnCharmFromTip(){
+ternaryCheckCity("NYC")
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+
+  switch (tip){
+    case 'generous':
+    return 'Thank you so much.'
+  case 'not as generous':
+    return 'Thank you.';
+    default:
+    return 'Bye.' 
+  }
+
 }
+
